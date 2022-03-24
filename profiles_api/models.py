c33d98fe3eb2fb99448d313201cda6fa826d6790
sofiat99 <52748797+sofiat99@users.noreply.github.com>
2022-03-24 12:11:53 -0500

    model user profile, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,55 @@
+from typing_extensions import Required
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user Profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
+        email = self.normalize_email(email)  # pone todo el correo en minuscula
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)  # es mejor poner la base de datos
+
+        return user
+
+    def create_super_user(self, email, name, password):
+        """Create a new super user profile with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Databasse model for users in the system """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_activate = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """return string representation of our user"""
+        return self.email
