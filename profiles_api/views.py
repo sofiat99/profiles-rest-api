f037511600b016bd88e74fca54a504b870e674f0
sofiat99 <52748797+sofiat99@users.noreply.github.com>
2022-03-25 17:36:04 -0500

    filter para buscar opr search in url

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,11 +3,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+# no deja put patch delete
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
-from profiles_api import permissions 
+from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     """Test API VIEW"""
@@ -111,3 +114,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',) #puedo filtrar por name or email en url es ?search=Nombre
